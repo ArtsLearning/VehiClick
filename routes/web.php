22f91f274b4f7aff39e67c0f3b20685c363132e6
ArtsLearning <arthursabs354@gmail.com>
2025-03-19 15:15:29 +0700

    merubah route web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/Home', function () {
+    return view('Home');
+});
+
+Route::get('/Login', function () {
+    return view('Login');
+});
+
+Route::get('/Register', function () {
+    return view('Register');
+});
+
+Route::get('/Dashboard', function () {
+    return view('Dashboard');
+});
+
+Route::get('/Product', function () {
+    return view('Product');
 });
